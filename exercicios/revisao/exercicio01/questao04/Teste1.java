278cc04d75e4bebd71b96f882f4c9b209390a695
Gabi <93677092+gaduarte@users.noreply.github.com>
2023-08-25 15:47:34 -0300

    Create Teste1.java

---
+++ exercicios/revisao/exercicio01/questao04/Teste1.java
@@ -0,0 +1,10 @@
+public class Teste1 {
+
+    public static void main(String[] args){
+        Area area = new Area(-1, 0);
+
+        area.validar();
+        System.out.println("Área válida.");
+    }
+    
+}
