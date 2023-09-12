4edc9f575a98bce87cb83689880a2d3792ea326d
Gabi <93677092+gaduarte@users.noreply.github.com>
2023-09-12 16:17:57 -0300

    Create Comentario.java

---
+++ exercicios/exercicio02/codigos/questao11/Comentario.java
@@ -0,0 +1,14 @@
+package questao11;
+
+public class Comentario {
+    private String texto;
+
+    public Comentario(String texto){
+        this.texto=texto;
+    }
+
+    public String getTexto(){
+        return texto;
+    }
+
+}
