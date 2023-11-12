408ecf087b89dac0c02ca3f9db9a9c57428dbb46
Gabi <93677092+gaduarte@users.noreply.github.com>
2023-11-12 15:35:35 -0300

    Add files via upload

---
+++ exercicios/exercico04/questao03/Ave.java
@@ -0,0 +1,20 @@
+package questao03;
+//Parece ser uma herança conveniente, porém pode deixar impressão de estar sendo mal aplicada.
+
+abstract class Ave {
+    public abstract void eat();
+}
+
+class Galinha extends Ave{
+    @Override
+    public void eat(){
+
+    }
+}
+
+class Pinguim extends Ave{
+    @Override
+    public void eat(){
+
+    }
+}
