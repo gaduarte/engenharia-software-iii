5fc5e77af7c6e95416001dd26677ecc44f703a55
Gabi <93677092+gaduarte@users.noreply.github.com>
2023-11-20 11:28:18 -0300

    Add files via upload

--- exercicios/exercicio05/questao04/Bird.java
+++ exercicios/exercicio05/questao04/Bird.java
@@ -1,27 +1,34 @@
-package questao04;
-// É uma forma conveniente de ser utilizado, porém deixa futuras expansões comprometidas ou com problemas de design, pois o código depende fortemente da verificação do tipo isFlightless() para o tratamento dos pássaro, ele só verifica se a ave voa ou não.
+package questão05;
+
+// Sim a composição poderia ser aplicada. Essa correção garante que classses de pássaros sem voo não herdem funcionalidade de voo de suas supercclasses.
 
 abstract class Bird {
-    public abstract void setLocation(double longitude, double latitude);
-    public abstract void setAltitude(double altitude);
     public abstract void draw();
+    public abstract void setLocation(double longitude, double latitude);
+}
+
+class FlightfulBird extends Bird {
+    private FlightfulBehavior flightfulBehavior;
+
+    public FlightfulBird(FlightfulBehavior flightfulBehavior){
+        this.flightfulBehavior = flightfulBehavior;
+    }
+    @Override
+    public void draw() {
+        // 
+    }
+
+    @Override
+    public void setLocation(double longitude, double latitude) {
+        // 
+    }
     
-    public abstract boolean isFlightless(); 
-
-
-void ArrangeBirdInPattern(Bird aBird) {
-    if (aBird.isFlightless()) {
-        ArrangeBirdOnGround(aBird);
-    } else {
-        ArrangeBirdInSky(aBird);
+    public void setAltitude(double altitude) {
+        // 
+    }
+    public void fly(){
+        flightfulBehavior.fly();
     }
 }
 
-void ArrangeBirdOnGround(Bird bird) {
-    //
-}
 
-void ArrangeBirdInSky(Bird bird) {
-    // 
-}
-}
