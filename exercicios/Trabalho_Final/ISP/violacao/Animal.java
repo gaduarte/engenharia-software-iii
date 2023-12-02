9f3cc31d01ccd537a1b5d53b7a9874603a7cac68
Gabi <93677092+gaduarte@users.noreply.github.com>
2023-12-02 17:49:34 -0300

    Create Animal.java

---
+++ exercicios/Trabalho_Final/ISP/violacao/Animal.java
@@ -0,0 +1,10 @@
+package ISP.violacao;
+
+public interface Animal {
+    void nada();
+    void voa();
+    void mamifero();
+    void vertebrado();
+    void invertebrado();
+    int tempoVida();
+}
