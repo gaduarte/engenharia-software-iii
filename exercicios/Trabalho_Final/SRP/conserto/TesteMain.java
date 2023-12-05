4cf0c1d5e8dca538094f62fe5289af57918e1fc9
Gabi <93677092+gaduarte@users.noreply.github.com>
2023-12-05 16:35:04 -0300

    Update TesteMain.java

--- exercicios/Trabalho_Final/SRP/conserto/TesteMain.java
+++ exercicios/Trabalho_Final/SRP/conserto/TesteMain.java
@@ -3,34 +3,39 @@ import java.time.LocalDate;
 
 public class TesteMain {
     public static void main(String[] args) {
-        LocalDate dataDevolucao = LocalDate.of(2023, 12, 2);
+        LocalDate dataDevolucao = LocalDate.of(2023, 12, 6);
 
         ClienteNovaVersao cliente = new ClienteNovaVersao(1, "Ana", 120);
         Comentario mensagem = new Comentario("ok!");
         Livro livro = new Livro("Livro A", 50, "Livro de suspense");
         Emprestimo emprestimo = new Emprestimo(dataDevolucao, true);
+        RenovarEmprestimo renovar = new RenovarEmprestimo();
+        DevolverLivro devolver = new DevolverLivro();
 
+        System.out.println("\n --- Detalhes do Cliente: ---");
         cliente.detalhesCliente();
         mensagem.comentarLivro(cliente);
+        System.out.println("\n --- Detalhes do Livro: ---");
         livro.detalhesLivro();
 
         try {
             emprestimo.realizarEmprestimodeLivro(cliente, livro);
-            System.out.println("Empréstimo realizado com sucesso!");
+            System.out.println(" \n Empréstimo realizado com sucesso!");
 
             // Exibir informações de empréstimo
-            System.out.println(emprestimo.informacoesEmprestimo(livro));
+            System.out.println("\n --- Informações do livro: --- \n" + emprestimo.informacoesEmprestimo(livro));
 
             // Tentar renovar o empréstimo
-            emprestimo.renovarEmprestimo();
+            renovar.renovarEmprestimo(emprestimo);
 
             // Devolver o livro
-            //emprestimo.devolverLivro();
-
-            // Verificar status novamente após devolução
+            //devolver.devolverLivro(emprestimo);
             emprestimo.verificarStatusEmprestimo();
+
         } catch (Exception e) {
             System.out.println("Erro ao realizar operação: " + e.getMessage());
+            e.printStackTrace(); // Imprimir o rastreamento completo da exceção
         }
     }
 }
+
